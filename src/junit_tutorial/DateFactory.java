47afb23c176aa0b8badb005a2d7d7852b947d2e4
mabuyoudev <mabu.you.dev@gmail.com>
2014-12-17 21:33:42 +0900

    委譲オブジェクトの抽出

---
+++ src/junit_tutorial/DateFactory.java
@@ -0,0 +1,9 @@
+package junit_tutorial;
+
+import java.util.Date;
+
+public class DateFactory {
+  Date newDate() {
+    return new Date();
+  }
+}
---
+++ src/junit_tutorial/DelegateObjectExample.java
@@ -0,0 +1,14 @@
+package junit_tutorial;
+
+import java.util.Date;
+
+public class DelegateObjectExample {
+  
+  DateFactory dateFactory = new DateFactory();
+  Date date = new Date();
+  
+  public void doSomething() {
+    this.date = dateFactory.newDate();
+  }
+  
+}
